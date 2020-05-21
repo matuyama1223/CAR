51b976b44aeebec8614d92297c13373568430d8a
matuyama1223 <matu1223@icloud.com>
2020-05-21 07:38:10 +0000

    [Add]caritem

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,13 +1,27 @@
 class Admin::ItemsController < ApplicationController
   def index
+  	@caritems = Caritem.all
+
   end
 
   def show
   end
 
   def new
+  	@caritem = Caritem.new
   end
 
   def edit
+  end
+
+  def create
+  	@caritem=Caritem.new(caritem_params)
+  	@caritem.save
+  	redirect_to admin_item_path(@caritem)
+  end
+
+  private
+  def caritem_params
+  	params.require(:caritem).permit(:name,:text,:price ,:carimage)
   end
 end
--- app/models/caritem.rb
+++ app/models/caritem.rb
@@ -1,2 +1,3 @@
 class Caritem < ApplicationRecord
+	attachment :carimage
 end
